a2e06eae38bd7cce2aa0bd4e31717366d90e2a1f
Damián Cabello <damian.cabello@nibw.es>
2022-10-12 14:26:39 +0200

    Añadida template para limpiar las vistas

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -1,54 +1,42 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <title>Image Gallery</title>
-    <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-    <!-- Fancybox -->
-    <script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.css" />
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+@extends('layouts.master')
 
-    <style>
-        .img-wraps {
-            position: relative;
-            display: inline-block;
-            font-size: 0;
+@section('title', 'Dashboard')
 
-        }
-        .img-wraps .delete-btn {
-            position: absolute;
-            right: -15px;
-            top: -20px;
-            padding: 5px 8px;
-            color: white;
-        }
+@section('style')
+.img-wraps {
+    position: relative;
+    display: inline-block;
+    font-size: 0;
 
-        .img-wraps .delete-btn button {
-            border-radius: 50%;
-        }
+}
+.img-wraps .delete-btn {
+    position: absolute;
+    right: -15px;
+    top: -20px;
+    padding: 5px 8px;
+    color: white;
+}
 
-        .img-wraps .update-btn {
-            position: absolute;
-            right: -5px;
-            top: 33px;
-            padding: 5px 8px;
-            color: white;
-            border-radius: 50%;
-            scale: 1.15;
-        }
-        .img-wraps:hover .delete-btn,
-        .img-wraps:hover .update-btn
-        {
-            opacity: 2;
-        }
-    </style>
-</head>
-<body>
-    <div class="container">
+.img-wraps .delete-btn button {
+    border-radius: 50%;
+}
+
+.img-wraps .update-btn {
+    position: absolute;
+    right: -5px;
+    top: 33px;
+    padding: 5px 8px;
+    color: white;
+    border-radius: 50%;
+    scale: 1.15;
+}
+.img-wraps:hover .delete-btn,
+.img-wraps:hover .update-btn
+{
+    opacity: 2;
+}
+@stop
+@section('content')
         <h3 class="mt-3 mb-2">Laravel - Gallery CRUD</h3>
         <form action="{{ url('gallery') }}" class="form-image-upload" method="POST" enctype="multipart/form-data">
             {!! csrf_field() !!}
@@ -118,15 +106,13 @@
                 </div>
             @endif
         </div>
-    </div>
-
-</body>
-<script>
+@stop
+@section('script')
     document.addEventListener("DOMContentLoaded", function(){
         var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
         var tooltipList = tooltipTriggerList.map(function(element){
             return new bootstrap.Tooltip(element);
         });
     });
-</script>
-</html>
+@stop
+
--- resources/views/image_info.blade.php
+++ resources/views/image_info.blade.php
@@ -1,25 +1,8 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <title>Image Gallery</title>
-    <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-    <!-- Fancybox -->
-    <script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.css" />
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+@extends('layouts.master')
 
-    <style>
+@section('title', 'Image Info')
 
-    </style>
-</head>
-<body>
-<div class="container">
-
-
+@section('content')
     <h3 class="mt-2 mb-5">Laravel - Ver/Editar imagen</h3>
     @if (count($errors) > 0)
         <div class="alert alert-danger alert-dismissible fade show">
@@ -77,9 +60,4 @@
             <a href="/" class="text-white btn btn-info btn-sm">Volver a la galería</a>
         </div>
     </div>
-</div>
-
-</body>
-<script>
-</script>
-</html>
+@stop
